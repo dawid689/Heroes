0c287c28d14037087f4a45d60bbb69de0d383450
Dawid <dsienkiewicz689@gmail.com>
2023-01-31 02:32:57 +0100

    Dashboard, heroes (list & details), messages

---
+++ heroes/src/app/hero.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HeroService {
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) {}
+
+  getHeroes(): Observable<Hero[]> {
+    // const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    // return heroes;
+
+    /** GET heroes from the server */
+    return this.http.get<Hero[]>(this.heroesUrl);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';
+}
